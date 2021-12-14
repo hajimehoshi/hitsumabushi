b8398ad6d8036bb819405d9f89d8a22c91aa8d79
Hajime Hoshi <hajimehoshi@gmail.com>
2021-12-14 12:11:17 +0000

    add test.sh and remove cmd/gooslibc

--- test.sh
+++ test.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-overlayJSON=$(go run genoverlayjson.go)
+overlayJSON=$(go run ./example/helloworld/genoverlayjson.go)
 env GOOS=linux GOARCH=arm64 CGO_ENABLED=1 \
     CGO_CFLAGS='-fno-common -fno-short-enums -ffunction-sections -fdata-sections -fPIC -g -O3' \
-    go build -buildmode=c-archive -overlay=$overlayJSON -o=helloworld.a
-gcc -o helloworld main.c helloworld.a -lpthread 
-./helloworld
+    go test -c -vet=off -overlay=$overlayJSON -o=test runtime
+./test -test.v -test.short
