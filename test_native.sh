35a5dab4de4d9bedde81c39959cecd7c038f5776
Hajime Hoshi <hajimehoshi@gmail.com>
2021-12-14 16:01:56 +0000

    add test_native.sh

---
+++ test_native.sh
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+go run ./genoverlayjson.go $1 > /tmp/overlay.json
+env GOOS=linux GOARCH=arm64 CGO_ENABLED=1 \
+    CGO_CFLAGS='-fno-common -fno-short-enums -ffunction-sections -fdata-sections -fPIC -g -O3' \
+    go test -c -vet=off -overlay=/tmp/overlay.json -o=test $1
+./test -test.v $2
