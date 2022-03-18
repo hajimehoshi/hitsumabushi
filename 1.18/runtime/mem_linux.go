3ba399ce4efe16fb57d0b2d31018a8a390754d2e
Hajime Hoshi <hajimehoshi@gmail.com>
2022-03-18 07:26:29 +0000

    support 1.18

---
+++ 1.18/runtime/mem_linux.go
@@ -0,0 +1,61 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This implementation is the most fundamental and minimal allocations like Wasm.
+// Malloced memory regions are never freed.
+
+package runtime
+
+import (
+	"internal/abi"
+	"unsafe"
+)
+
+// Don't split the stack as this function may be invoked without a valid G,
+// which prevents us from allocating more stack.
+//go:nosplit
+func sysAlloc(n uintptr, sysStat *sysMemStat) unsafe.Pointer {
+	p := sysReserve(nil, n)
+	sysMap(p, n, sysStat)
+	return p
+}
+
+func sysUnused(v unsafe.Pointer, n uintptr) {
+}
+
+func sysUsed(v unsafe.Pointer, n uintptr) {
+}
+
+func sysHugePage(v unsafe.Pointer, n uintptr) {
+}
+
+// Don't split the stack as this function may be invoked without a valid G,
+// which prevents us from allocating more stack.
+//go:nosplit
+func sysFree(v unsafe.Pointer, n uintptr, sysStat *sysMemStat) {
+	sysStat.add(-int64(n))
+}
+
+func sysFault(v unsafe.Pointer, n uintptr) {
+}
+
+func sysReserve(v unsafe.Pointer, n uintptr) unsafe.Pointer {
+	if v != nil {
+		return nil
+	}
+	ptr := calloc(n, 1)
+	return unsafe.Pointer(ptr)
+}
+
+//go:nosplit
+//go:cgo_unsafe_args
+func calloc(n uintptr, size uintptr) (ret uintptr) {
+	libcCall(unsafe.Pointer(abi.FuncPCABI0(calloc_trampoline)), unsafe.Pointer(&n))
+	return
+}
+func calloc_trampoline(n uintptr, size uintptr) uintptr
+
+func sysMap(v unsafe.Pointer, n uintptr, sysStat *sysMemStat) {
+	sysStat.add(int64(n))
+}
