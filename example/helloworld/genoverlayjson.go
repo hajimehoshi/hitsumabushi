72619ec063509848263ad761108e09f77d79e1d7
Hajime Hoshi <hajimehoshi@gmail.com>
2022-08-26 20:57:28 +0900

    example/helloworld: bug fix: build failure

---
+++ example/helloworld/genoverlayjson.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 Hajime Hoshi
+
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hajimehoshi/hitsumabushi"
+)
+
+func main() {
+	if err := build(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func build() error {
+	options := []hitsumabushi.Option{
+		hitsumabushi.Args(os.Args...),
+		hitsumabushi.GOOS("linux"),
+	}
+	overlayJSON, err := hitsumabushi.GenOverlayJSON(options...)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stdout.Write(overlayJSON); err != nil {
+		return err
+	}
+	return nil
+}
--- example/helloworld/run.sh
+++ example/helloworld/run.sh
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2022 Hajime Hoshi
 
 set -e
 
-go run ../../genoverlayjson.go > /tmp/overlay.json
+go run genoverlayjson.go > /tmp/overlay.json
 env GOOS=linux GOARCH=arm64 CGO_ENABLED=1 \
     CGO_CFLAGS='-fno-common -fno-short-enums -ffunction-sections -fdata-sections -fPIC -g -O3' \
     go build -buildmode=c-archive -overlay=/tmp/overlay.json -o=helloworld.a
