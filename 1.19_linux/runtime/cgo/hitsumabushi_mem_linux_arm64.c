f31d4bbafef1c258b582bb8e23ac13edc7978d78
Hajime Hoshi <hajimehoshi@gmail.com>
2022-10-06 02:00:12 +0900

    1.19_linux/runtime, 1.19_linux/runtime/cgo: create a separate file for memory
    Updates #3

---
+++ 1.19_linux/runtime/cgo/hitsumabushi_mem_linux_arm64.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+void* hitsumabushi_sysReserveOS(void* v, uintptr_t n);
+
+void* hitsumabushi_sysAllocOS(uintptr_t n) {
+  return hitsumabushi_sysReserveOS(NULL, n);
+}
+
+void hitsumabushi_sysUnusedOS(void* v, uintptr_t n) {
+}
+
+void hitsumabushi_sysUsedOS(void* v, uintptr_t n) {
+}
+
+void hitsumabushi_sysHugePageOS(void* v, uintptr_t n) {
+}
+
+void hitsumabushi_sysFreeOS(void* v, uintptr_t n) {
+}
+
+void hitsumabushi_sysFaultOS(void* v, uintptr_t n) {
+}
+
+void* hitsumabushi_sysReserveOS(void* v, uintptr_t n) {
+  if (v) {
+    return NULL;
+  }
+  return calloc(n, 1);
+}
+
+void hitsumabushi_sysMapOS(uintptr_t n, uintptr_t size) {
+}
