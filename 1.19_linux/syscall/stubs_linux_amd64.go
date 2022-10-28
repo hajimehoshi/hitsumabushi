f51651a7a3f691f8f24a89973f53e4f9d4561288
Sam Hocevar <sam@hocevar.net>
2022-10-28 14:25:21 +0200

    Ensure the fstatat() syscall stub can compile on amd64

---
+++ 1.19_linux/syscall/stubs_linux_amd64.go
@@ -0,0 +1,5 @@
+package syscall
+
+const (
+	SYS_FSTATAT = 0xffffffffffffffff // Stub: on amd64 Go uses SYS_NEWFSTATAT instead
+)
---
+++ 1.19_linux/syscall/stubs_linux_arm64.go
@@ -0,0 +1,5 @@
+package syscall
+
+const (
+	SYS_NEWFSTATAT = 0xffffffffffffffff // Stub: on arm64 Go uses SYS_FSTATAT instead
+)
