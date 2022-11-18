f5cf3e2eca525b879602952a6fbeb30219452150
Sam Hocevar <sam@hocevar.net>
2022-11-18 17:50:37 +0100

    Remove unused libc stubs to get rid of <signal.h>

--- 1.19_linux/runtime/cgo/hitsumabushi_syscalls_linux.c
+++ 1.19_linux/runtime/cgo/hitsumabushi_syscalls_linux.c
@@ -3,108 +3,18 @@
 
 // This file defines C functions and system calls for Cgo.
 
-#include <pthread.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <stdatomic.h>
+#include <pthread.h> // for pthread_self
+#include <stdint.h> // for uint32_t etc.
+#include <stdlib.h> // for exit()
 #include <unistd.h> // for usleep
 #include <stddef.h> // for size_t
-#include <signal.h> // for sigset_t and struct sigaction
+#include <time.h> // for struct timespec
 
 #include "libcgo.h"
 #include "libcgo_unix.h"
 
-typedef unsigned int gid_t;
-
 extern int hitsumabushi_clock_gettime(clockid_t clk_id, struct timespec *tp);
 extern int32_t hitsumabushi_getproccount();
-
-void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
-  abort();
-  return NULL;
-}
-
-int munmap(void *addr, size_t length) {
-  abort();
-  return 0;
-}
-
-int pthread_sigmask(int how, const sigset_t *set, sigset_t *oldset) {
-  // Do nothing.
-  return 0;
-}
-
-int setegid(gid_t gid) {
-  // Do nothing.
-  return 0;
-}
-
-int seteuid(uid_t gid) {
-  // Do nothing.
-  return 0;
-}
-
-int setgid(gid_t gid) {
-  // Do nothing.
-  return 0;
-}
-
-int setgroups(size_t size, const gid_t *list) {
-  // Do nothing.
-  return 0;
-}
-
-int setregid(gid_t rgid, gid_t egid) {
-  // Do nothing.
-  return 0;
-}
-
-int setreuid(uid_t ruid, uid_t euid) {
-  // Do nothing.
-  return 0;
-}
-
-int setresgid(gid_t rgid, gid_t egid, gid_t sgid) {
-  // Do nothing.
-  return 0;
-}
-
-int setresuid(uid_t ruid, uid_t euid, uid_t suid) {
-  // Do nothing.
-  return 0;
-}
-
-int setuid(uid_t gid) {
-  // Do nothing.
-  return 0;
-}
-
-int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) {
-  // Do nothing.
-  return 0;
-}
-
-int sigaddset(sigset_t *set, int signum) {
-  // Do nothing.
-  return 0;
-}
-
-int sigemptyset(sigset_t *set) {
-  // Do nothing.
-  return 0;
-}
-
-int sigfillset(sigset_t *set) {
-  // Do nothing.
-  return 0;
-}
-
-int sigismember(const sigset_t *set, int signum) {
-  // Do nothing.
-  return 0;
-}
 
 uint32_t hitsumabushi_gettid() {
   uint64_t tid64 = (uint64_t)(pthread_self());
