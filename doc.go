12df12138d6c48bb9b80d1547f4cfd20fb753fff
Hajime Hoshi <hajimehoshi@gmail.com>
2025-06-12 23:47:10 +0900

    hitsumabushi: update documents

--- doc.go
+++ doc.go
@@ -3,5 +3,6 @@
 
 // Package hitsumabushi provides APIs to generate JSON for go-build's `-overlay` option.
 // Hitsumabushi aims to make Go programs work on almost everywhere by overwriting system calls with C function calls.
-// Now the generated JSON works only for Arm64 so far.
+// Now the generated JSON works only for Linux/Amd64, Linux/Arm64, and Windows/Amd64 so far.
+// For GOOS=windows, Hitsumabushi replaces some functions that don't work on some special Windows-like systems.
 package hitsumabushi
